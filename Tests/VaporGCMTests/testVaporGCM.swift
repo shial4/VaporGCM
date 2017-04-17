6844b17d882e01f036df65f61359f930bc61379e
Shial <shial@bitbucket.org>
2017-04-17 13:14:58 +1000

    [T-001] update tests

--- Tests/VaporGCMTests/testVaporGCM.swift
+++ Tests/VaporGCMTests/testVaporGCM.swift
@@ -16,9 +16,17 @@ class testVaporGCM: XCTestCase {
         ("testGCM", testGCM),
         ("testNotification", testNotification),
         ("testNotificationToJSON", testNotificationToJSON),
+        ("testNotificationFields", testNotificationFields),
+        ("testNotificationMessage", testNotificationMessage),
+        ("testNotificationBodyWithTitle", testNotificationBodyWithTitle),
         ("testPushMessage", testPushMessage),
+        ("testPushMessageFields", testPushMessageFields),
         ("testSendMessageToDevice", testSendMessageToDevice),
-        ("testSendMessageToMultipleDevices", testSendMessageToMultipleDevices)
+        ("testSendMessageToMultipleDevices", testSendMessageToMultipleDevices),
+        ("testDeviceGroup", testDeviceGroup),
+        ("testNoDroplet", testNoDroplet),
+        ("testNoDroplet", testNoDroplet2),
+        ("testNoDroplet", testNoDroplet3)
     ]
     
     var drop: Droplet! = nil
@@ -43,6 +51,32 @@ class testVaporGCM: XCTestCase {
         XCTAssertNotNil(gcmPayload)
     }
     
+    func testNotificationMessage() {
+        let gcmPayload = GCMPayload(message: "message")
+        do {
+            let json = try gcmPayload.makeJSON()
+            let value: String = try json.extract("body")
+            XCTAssertNotNil(value)
+        } catch {
+            XCTFail()
+        }
+        XCTAssertNotNil(gcmPayload)
+    }
+    
+    func testNotificationBodyWithTitle() {
+        let gcmPayload = GCMPayload(title: "title", body: "body")
+        do {
+            let json = try gcmPayload.makeJSON()
+            let value1: String = try json.extract("title")
+            XCTAssertNotNil(value1)
+            let value2: String = try json.extract("body")
+            XCTAssertNotNil(value2)
+        } catch {
+            XCTFail()
+        }
+        XCTAssertNotNil(gcmPayload)
+    }
+    
     func testNotificationToJSON() {
         let gcmPayload = GCMPayload()
         var json: JSON?
@@ -54,6 +88,39 @@ class testVaporGCM: XCTestCase {
         XCTAssertNotNil(json)
     }
     
+    func testNotificationFields() {
+        var gcmPayload = GCMPayload()
+        gcmPayload.title = "title"
+        gcmPayload.body = "body"
+        gcmPayload.sound = "sound"
+        gcmPayload.tag = "tag"
+        gcmPayload.color = "color"
+        gcmPayload.clickAction = "click_action"
+        gcmPayload.bodyLocKey = "body_loc_key"
+        gcmPayload.bodyLocArgs = JSON([:])
+        gcmPayload.titleLocKey = JSON([:])
+        gcmPayload.titleLocArgs = JSON([:])
+        do {
+            let json = try gcmPayload.makeJSON()
+            try ["title","body","sound","tag","color","click_action","body_loc_key"].forEach() { key in
+                let value: String = try json.extract(key)
+                XCTAssertNotNil(value)
+            }
+        } catch {
+            XCTFail()
+        }
+        do {
+            let json = try gcmPayload.makeJSON()
+            try ["body_loc_args","title_loc_key","title_loc_args"].forEach() { key in
+                let value: JSON = try json.extract(key)
+                XCTAssertNotNil(value)
+            }
+        } catch {
+            XCTFail()
+        }
+        XCTAssertNotNil(gcmPayload)
+    }
+    
     func testPushMessage() {
         var message: PushMessage?
         do {
@@ -61,6 +128,55 @@ class testVaporGCM: XCTestCase {
         } catch {
             XCTFail()
         }
+        XCTAssertNotNil(message)
+    }
+    
+    func testPushMessageFields() {
+        var message: PushMessage?
+        let gcmPayload = GCMPayload()
+        let data = JSON([
+            "score":"5x1",
+            "time":"15:10"
+            ])
+        do {
+            message = try PushMessage(gcmPayload: gcmPayload, data: data)
+        } catch {
+            XCTFail()
+        }
+        message?.priority = GCMPMessagePriority.high
+        message?.timeToLive = 5
+        message?.collapseKey = "Collapse"
+        message?.restrictedPackageName = "Package"
+        message?.dryRun = true
+        
+        do {
+            let json = try message?.makeJSON(recipient: "token11")
+            
+            let value1: JSON? = try json?.extract("notification")
+            XCTAssertNotNil(value1)
+            
+            let value2: JSON? = try json?.extract("data")
+            XCTAssertNotNil(value2)
+            
+            let value3: String? = try json?.extract("priority")
+            XCTAssertTrue(value3 == "high")
+            
+            let value4: Int? = try json?.extract("time_to_live")
+            XCTAssertTrue(value4 == 5)
+            
+            let value5: String? = try json?.extract("collapse_key")
+            XCTAssertTrue(value5 == "Collapse")
+            
+            let value6: String? = try json?.extract("restricted_package_name")
+            XCTAssertTrue(value6 == "Package")
+            
+            let value7: Bool? = try json?.extract("dry_run")
+            XCTAssertTrue(value7 == true)
+            
+        } catch {
+            XCTFail()
+        }
+        
         XCTAssertNotNil(message)
     }
     
@@ -98,5 +214,62 @@ class testVaporGCM: XCTestCase {
             XCTFail()
         }
         self.waitForExpectations(timeout: 10.0, handler: nil)
+    }
+    
+    func testDeviceGroup() {
+        let gcm = VaporGCM(forDroplet: drop, serverKey: "bk3RNwTe3H0:CI2k_HHwgIpoDKCIZvvDMExUdFQ3P1...")
+        let group = DeviceGroup(operation: .create,
+                                name: "appUser-Chris",
+                                registrationIds: ["16", "9"])
+        let response = try? gcm.sendDeviceGroup(group, forProject: "SENDER_ID")
+        XCTAssertNotNil(response)
+    }
+    
+    func testNoDroplet() {
+        var drop: Droplet! = self.drop
+        let gcm = VaporGCM(forDroplet: drop, serverKey: "bk3RNwTe3H0:CI2k_HHwgIpoDKCIZvvDMExUdFQ3P1...")
+        drop = nil
+        do {
+            let message = try PushMessage()
+            let response = try gcm.send(message, to: "bk3RNwTe3H0:CI2k_HHwgIpoDKCIZvvDMExUdFQ3P1...")
+            XCTAssertNotNil(response)
+        } catch InitializeError.missingDroplet {
+            print("missingDroplet")
+            XCTAssertTrue(true)
+        } catch {
+            XCTFail()
+        }
+    }
+    
+    func testNoDroplet2() {
+        var drop: Droplet! = self.drop
+        let gcm = VaporGCM(forDroplet: drop, serverKey: "bk3RNwTe3H0:CI2k_HHwgIpoDKCIZvvDMExUdFQ3P1...")
+        drop = nil
+        do {
+            let message = try PushMessage()
+            try gcm.send(message, to: ["1","2","3"], responseHandler: nil)
+        } catch InitializeError.missingDroplet {
+            print("missingDroplet")
+            XCTAssertTrue(true)
+        } catch {
+            XCTFail()
+        }
+    }
+    
+    func testNoDroplet3() {
+        var drop: Droplet! = self.drop
+        let gcm = VaporGCM(forDroplet: drop, serverKey: "bk3RNwTe3H0:CI2k_HHwgIpoDKCIZvvDMExUdFQ3P1...")
+        drop = nil
+        do {
+            let group = DeviceGroup(operation: .create,
+                                    name: "appUser-Chris",
+                                    registrationIds: ["16", "9"])
+            _ = try gcm.sendDeviceGroup(group, forProject: "SENDER_ID")
+        } catch InitializeError.missingDroplet {
+            print("missingDroplet")
+            XCTAssertTrue(true)
+        } catch {
+            XCTFail()
+        }
     }
 }
