724ad6793bceb8871d5660a660bac3f06bbb8dce
Shial <shial184686@gmail.com>
2017-05-18 09:37:20 +1000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "VaporGCM",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", Version(2,0,0, prereleaseIdentifiers: ["beta"]))
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2)
     ],
     exclude: [
         "Config",
